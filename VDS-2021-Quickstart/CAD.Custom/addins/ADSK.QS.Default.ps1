f21a9d1bab6f9ec3bebf6bcd47114fadda757acf
koechlm <markus.koechl@autodesk.com>
2020-04-27 09:43:37 +0200

    optimized performance for AutoCAD ListValues issue

--- VDS-2021-Quickstart/CAD.Custom/addins/ADSK.QS.Default.ps1
+++ VDS-2021-Quickstart/CAD.Custom/addins/ADSK.QS.Default.ps1
@@ -243,6 +243,7 @@ function InitializeWindow
 		"AutoCADWindow"
 		{
 			#workaround the listvalue issue of 2021 RTM
+			$global:CatChangedSubscribed = $false
 			mEnableListValues
 
 			InitializeBreadCrumb
@@ -981,35 +982,20 @@ function mInitializeCHContext {
 #workaround the listvalue issue of 2021 RTM
 function mEnableListValues
 {
+	if (-not $global:CatChangedSubscribed)
+    {
+        $Prop["_Category"].add_PropertyChanged({
+		    mEnableListValues
+	    })
+        $global:CatChangedSubscribed = $true
+    }
+
 	$dC = $dsWindow.DataContext
-	
-	if($Prop["_EditMode"].Value -eq $true)
-	{
-		$dC.Properties.Properties | ForEach-Object{
-			if(-not $_.ListValues.Count -and $_.PropDefInfo.ListValArray.Count)
-			{
-				$_.ListValues = $_.PropDefInfo.ListValArray
-			}
+	$dC.Properties.Properties | ForEach-Object{
+		if(-not $_.ListValues.Count -and $_.PropDefInfo.ListValArray.Count)
+		{
+			$_.ListValues = $_.PropDefInfo.ListValArray
 		}
 	}
 
-	if($Prop["_CreateMode"].Value -eq $true)
-	{
-		Add-Type -Path "C:\Program Files\Autodesk\Vault Client 2021\Explorer\Autodesk.DataManagement.Client.Framework.Vault.dll"
-		$propDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId("FILE")
-		$propDefDic = @{}
-		$propDefs | ForEach-Object{
-			$propDefDic.Add($_.DispName, $_.Id)
-		}
-		
-		$dC.Properties.Properties | ForEach-Object{
-			$PropertyDefinition = New-Object Autodesk.DataManagement.Client.Framework.Vault.Currency.Properties.PropertyDefinition
-			$id = $propDefDic.Get_Item($_.DispName)
-			$PropertyDefinition = $vaultConnection.PropertyManager.GetPropertyDefinitionById($id)
-			if($PropertyDefinition.HasListValues -eq $true)
-			{
-				$_.ListValues = ($PropertyDefinition.ValueList | Foreach-Object{ $_.Value})
-			}
-		}
-	}
 }
